b4da94495a41d5cd14bb8fc70762fbb4c9766d48
Petro Mozil <mozil.petryk@gmail.com>
2025-03-28 07:24:10 +0000

    Add glm
    Signed-off-by: Petro Mozil <mozil.petryk@gmail.com>

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-# Taken from: https://github.com/graphdeco-inria/diff-gaussian-rasterization/blob/main/setup.py
-
 from setuptools import setup
 from torch.utils.cpp_extension import CUDAExtension, BuildExtension
 import os
@@ -10,13 +8,10 @@ setup(
     packages=['gsplat_raytracing'],
     ext_modules=[
         CUDAExtension(
-            name="gsplat_raytracing._C",
+            name="gsplat_raytracing.cu",
             sources=[
-            "cuda_rasterizer/rasterizer_impl.cu",
-            "cuda_rasterizer/forward.cu",
-            "cuda_rasterizer/backward.cu",
-            "rasterize_points.cu",
-            "ext.cpp"],
+            "src/main.cu",
+            "src/ext.cpp"],
             extra_compile_args={"nvcc": ["-I" + os.path.join(os.path.dirname(os.path.abspath(__file__)), "third_party/glm/")]})
         ],
     cmdclass={
---
+++ src/ext.cpp
@@ -0,0 +1,4 @@
+#include <torch/extension.h>
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+}
